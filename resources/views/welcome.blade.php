30fe062704ffc0819cbda4d0608dd7f30d3eefde
deeluxe74 <rollet.raphael@gmail.com>
2020-05-16 06:22:40 +0200

    mas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -26,7 +26,7 @@
         <script src="{{ asset('js/app.js') }}" defer></script>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
         
-        <title>LoverWantCashYeS</title>
+        <title>LoverWantCash</title>
         
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Mali:200,300,400,500,600,700&display=swap" rel="stylesheet">
